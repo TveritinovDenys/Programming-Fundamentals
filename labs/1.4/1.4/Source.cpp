e36aac9e8c6f641201d56124f274cc5759d46489
TveritinovDenys <tveritinovdenis8@gmail.com>
2024-09-30 14:59:33 +0300

    finish_1.4lab

---
+++ labs/1.4/1.4/Source.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+int main() {
+	//usigned short
+	unsigned short A;
+	unsigned short* pA;
+	pA = &A;
+	*pA = 524;
+
+	void* pVoid;
+	pVoid = &A;
+	pVoid = pA;
+
+	//int
+	int B;
+	int* pB;
+	pB = &B;
+	*pB = -61;
+
+	pVoid = &B;
+	pVoid = pB;
+
+	//float
+	float C;
+	float* pC;
+	pC = &C;
+	*pC = 25.7295;
+
+	pVoid = &C;
+	pVoid = pC;
+
+	//double
+	double D;
+	double* pD;
+	pD = &D;
+	*pD = -0.53e-707;
+
+	pVoid = &D;
+	pVoid = pD;
+
+
+
+
+
+
+
+
+
+}
