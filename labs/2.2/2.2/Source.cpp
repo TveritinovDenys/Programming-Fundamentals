0b8b4e123ae65e51a18da04041d1d94433a82406
TveritinovDenys <tveritinovdenis8@gmail.com>
2024-10-14 22:24:23 +0300

    finishe_1task_2.2lab

---
+++ labs/2.2/2.2/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+enum Names { Andrew, Vlad, Denys, Igor, Maksym, Olga, Dasha };
+struct student {
+	Names name;
+	int yearOfStudy;
+	bool stipend;
+};
+
+int main() {
+	student stdArr[7];
+
+	stdArr[0] = { Andrew, 1, false };
+	stdArr[1] = { Vlad, 1, true };
+	stdArr[2] = { Denys,1,true };
+	stdArr[3] = { Igor, 4, false };
+	stdArr[4] = { Maksym, 1, false };
+	stdArr[5] = { Olga, 1, false };
+	stdArr[6].name = Dasha;
+	stdArr[6].yearOfStudy = 2;
+	stdArr[6].stipend = true;
+
+	int count = 0;
+	for (int i = 0; i < 7; i++) {
+		if (stdArr[i].yearOfStudy == 1 && stdArr[i].stipend == 1) {
+			count++;
+		}
+	}
+	
+	float result = (float)count * 100 / 7;
+	std::cout << result << std::endl;
+}
