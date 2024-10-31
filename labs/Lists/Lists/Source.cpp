e0ad248d2ae7fbced01989e850439501a2d2b688
TveritinovDenys <tveritinovdenis8@gmail.com>
2024-11-01 00:00:31 +0200

    added_comments

--- labs/Lists/Lists/Source.cpp
+++ labs/Lists/Lists/Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 struct Node {
 	int data;
@@ -30,6 +30,7 @@ inline void printList(Node* head) {
 }
 
 inline void addToEnd(Node** head, int value) {
+    // Перевірка, чи список пустий
     if (*head == nullptr) {
         addToFront(head, value);
         return;
@@ -48,60 +49,75 @@ inline void addToEnd(Node** head, int value) {
     }
 }
 
+// Функція для видалення останнього елемента з пов'язаного списку
 inline void removeFromEnd(Node** head) {
-    if (*head == nullptr)
+    // Перевірка, чи список пустий
+    if (*head == nullptr) {
         return;
+    }
+    // Якщо в списку тільки один елемент
     else if ((*head)->next == nullptr) {
-        delete* head;
+        delete* head; 
         *head = nullptr;
         return;
     }
+    // Перевірка на випадок, якщо список не пустий
     else if (*head != nullptr) {
-        Node* current = *head;
+        Node* current = *head; 
+        // Проходимо до передостаннього елемента
         while (current->next->next != nullptr) {
-            current = current->next;
+            current = current->next; 
         }
 
-        delete current->next;
-        current->next = nullptr;
+        delete current->next; // Видаляємо останній елемент
+        current->next = nullptr; // Оновлюємо вказівник на наступний елемент як nullptr
     }
 }
 
+// Функція для пошуку елемента за значенням
 inline Node* searchElement(Node** head, int value) {
+    // Перевірка, чи список не пустий
     if (*head != nullptr) {
-        Node* current = *head;
+        Node* current = *head; 
 
+        // Проходимо по всіх елементах списку
         while (current != nullptr) {
+            // Перевіряємо, чи значення поточного елемента дорівнює шуканому
             if (current->data == value)
                 return current;
 
-            current = current->next;
+            current = current->next; 
         }
     }
-    
-    return nullptr;
+
+    return nullptr; // Якщо елемент не знайдено, повертаємо nullptr
 }
+
 
 int main() {
     Node* head = nullptr;
     
+    // Додаємо елементи до списку
     addToEnd(&head, 10);
     addToEnd(&head, 20);
     addToFront(&head, 0);
     addToEnd(&head, 30);
 
+    // Виводимо список
     printList(head);
 
+    // Шукаємо елемент за значенням
     std::cout << searchElement(&head, 20) << std::endl;
 
+    // Видаляємо на 1 елемент більше, ніж було в списку
     removeFromEnd(&head);
     removeFromEnd(&head);
     removeFromEnd(&head);
     removeFromEnd(&head);
     removeFromEnd(&head);
-    std::cout << searchElement(&head, 20) << std::endl;
-    printList(head);
 
+    // Шукаємо неіснуючий елемент
+    std::cout << searchElement(&head, 20) << std::endl;
 
     return 0;
 }
